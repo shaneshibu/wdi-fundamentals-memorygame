4f353dc3bfb6a299b10f3092f2132a3d30156a5a
Shane Shibu <shane.shibu.09@gmail.com>
2019-05-01 17:36:43 +0100

    changed cards array to hold objects instead of strings

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,25 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png",
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png",
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png",
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png",
+	},
+];
 
 var cardsInPlay = [];
 
@@ -12,9 +33,11 @@ function checkForMatch() {
 
 function flipCard(cardID) {
 
-	console.log("User flipped " + cards[cardID]);
+	console.log("User flipped " + cards[cardID].rank);
+	console.log(cards[cardID].suit);
+	console.log(cards[cardID].cardImage);
 
-	cardsInPlay.push(cards[cardID]);
+	cardsInPlay.push(cards[cardID].rank);
 
 	if (cardsInPlay.length===2) {
 		checkForMatch();
