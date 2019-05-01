e73742f3f20240deeafecad0b0dd74f6838b301e
Shane Shibu <shane.shibu.09@gmail.com>
2019-05-01 21:42:18 +0100

    added score counter

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -22,13 +22,29 @@ var cards = [
 ];
 
 var cardsInPlay = [];
+var score = 0;
+
+function resetBoard() {
+	
+	var cardElements = document.getElementsByTagName("img");
+
+	for (var i=0; i<cards.length; i++) {
+    	cardElements[i].setAttribute("src", "images/back.png");
+	}
+
+	cardsInPlay = [];
+}
 
 function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 	  alert("You found a match!");
+	  score ++;
+	  document.getElementById("score").innerHTML = "Score: " + score;
 	} else {
 	  alert("Sorry, try again.");
 	}
+
+	resetBoard();
 }
 
 function flipCard() {
@@ -47,7 +63,7 @@ function flipCard() {
 function createBoard() {
 
 	for (var i=0; i<cards.length; i++) {
-    	var cardElement = document.createElement('img');
+    	var cardElement = document.createElement("img");
     	cardElement.setAttribute("src", "images/back.png");
     	cardElement.setAttribute("data-id", i);
     	cardElement.addEventListener("click", flipCard);
